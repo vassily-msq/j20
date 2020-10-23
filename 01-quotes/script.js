0b6a801757ec06ba2629dfcdcc2a14c5179465bf
vl <vasil.deville@gmail.com>
2020-10-23 09:33:26 +0300

    Fiddling w proxies

--- 01-quotes/script.js
+++ 01-quotes/script.js
@@ -1,1 +1,23 @@
-console.log("testing");
+// console.log("testing");
+
+// Get quote from API
+async function getQuote() {
+  // Call a proxi api forst
+  // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const proxyUrl = "https://yacdn.org/";
+  const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    // Use both api Url and proxy urls
+    const response = await fetch(proxyUrl + apiUrl);
+    // const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    
+  } catch (error) {
+    // getQuote();
+    console.log('Whoops, no quote!', error);
+  }
+
+}
+
+getQuote();
